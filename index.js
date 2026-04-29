a7243cc5537f2c071e662123b822c7fdbd2cc29d
juliapalmacunha <juliapcunha13@gmail.com>
2026-04-29 11:02:06 -0300

    chore: add pg and dotenv setup

---
+++ index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config.js'
+import express from 'express'
+import { postgresHelper } from './src/db/postgres/helper.js'
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    const results = await postgresHelper.query('SELECT * FROM users')
+    res.send(JSON.stringify(results))
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
---
+++ src/db/postgres/helper.js
@@ -0,0 +1,20 @@
+import pg from 'pg'
+
+const { Pool } = pg
+
+const pool = new Pool({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+})
+
+export const postgresHelper = {
+    query: async (text, params) => {
+        const client = await pool.connect()
+        const results = await client.query(text, params)
+        await client.release()
+        return results.rows
+    },
+}
