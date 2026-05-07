6b8c129650f531c83f283c8c8853030aa8bcf5da
juliapalmacunha <juliapcunha13@gmail.com>
2026-05-07 16:16:10 -0300

    feat: add GetUserByIdController and validations

---
+++ src/controllers/get-user-by-id.js
@@ -0,0 +1,22 @@
+import { GetUserByIdUseCase } from '../use-cases/get-user-by-id.js'
+import { badRequest, ok, serverError } from './helpers.js'
+import validator from 'validator'
+
+export class GetUserByIdController {
+    async execute(httpRequest) {
+        try {
+            const isIdValid = validator.isUUID(httpRequest.params.id)
+            if (!isIdValid) {
+                return badRequest({ message: 'Invalid user ID format' })
+            }
+
+            const getUserByIdUseCase = new GetUserByIdUseCase()
+            const user = await getUserByIdUseCase.execute(httpRequest.params.id)
+
+            return ok(user)
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
+    }
+}
--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -1,20 +1,19 @@
-export const badRequest = (body) => {
-    return {
-        statusCode: 400,
-        body,
-    }
-}
+export const badRequest = (body) => ({
+    statusCode: 400,
+    body,
+})
 
-export const created = (body) => {
-    return {
-        statusCode: 201,
-        body,
-    }
-}
+export const created = (body) => ({
+    statusCode: 201,
+    body,
+})
 
-export const serverError = (body) => {
-    return {
-        statusCode: 500,
-        body,
-    }
-}
+export const serverError = (body) => ({
+    statusCode: 500,
+    body: body,
+})
+
+export const ok = (body) => ({
+    statusCode: 200,
+    body,
+})
