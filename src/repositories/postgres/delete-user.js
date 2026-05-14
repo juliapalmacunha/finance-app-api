0765b9055cf2b6754739ac67095d9907d73f047f
juliapalmacunha <juliapcunha13@gmail.com>
2026-05-14 10:16:53 -0300

    fix: rename PostgresDeleteUser to PostgresDeleteUserRepository

--- src/repositories/postgres/delete-user.js
+++ src/repositories/postgres/delete-user.js
@@ -1,6 +1,6 @@
 import { PostgresHelper } from '../../db/postgres/helper'
 
-export class PostgresDeleteUser {
+export class PostgresDeleteUserRepository {
     execute(userId) {
         const deleteUser = PostgresHelper.query(
             'DELETE FROM users WHERE id = $1 RETURNING *',
