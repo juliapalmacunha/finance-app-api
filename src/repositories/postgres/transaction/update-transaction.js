2c986337082c33a38c282857e1b5fba1f313600a
juliapalmacunha <juliapcunha13@gmail.com>
2026-05-20 10:43:56 -0300

    feat: add PostgresUpdateTransactionRepository

---
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -0,0 +1,31 @@
+import { PostgresHelper } from '../../../db/postgres/helper'
+
+export class PostgresUpdateTransactionRepository {
+    async execute(userId, updateTransactionParams) {
+        //criando listas para que os parametros
+        // entrem na posição certa e isso seja enviado para o postgres em linguagem sql
+        const updateFields = []
+        const updateValues = []
+
+        Object.keys(updateTransactionParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateFields.length + 1}`)
+            updateValues.push(updateTransactionParams[key])
+        })
+
+        updateValues.push(userId)
+
+        const updateQuery = `
+            UPDATE transactions 
+            SET ${updateFields.join(', ')} 
+            WHERE id = $${updateValues.length}
+            RETURNING *
+            `
+
+        const updatedTransaction = await PostgresHelper.query(
+            updateQuery,
+            updateValues,
+        )
+
+        return updatedTransaction[0]
+    }
+}
