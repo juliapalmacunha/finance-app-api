c9d34645ba0186842f0fc934ac27c54751b084a5
juliapalmacunha <juliapcunha13@gmail.com>
2026-05-20 12:59:30 -0300

    feat: add makeDeleteTransactionController in factory file

--- src/factories/controllers/transaction.js
+++ src/factories/controllers/transaction.js
@@ -1,16 +1,19 @@
 import {
     CreateTransactionController,
+    DeleteTransactionController,
     GetTransactionsByUserIdController,
     UpdateTransactionController,
 } from '../../controllers/index.js'
 import {
     PostgresCreateTransactionRepository,
+    PostgresDeleteTransactionRepository,
     PostgresGetTransactionsByUserIdRepository,
     PostgresGetUserByIdRepository,
     PostgresUpdateTransactionRepository,
 } from '../../repositories/postgres/index.js'
 import {
     CreateTransactionUseCase,
+    DeleteTransactionUseCase,
     GetTransactionsByUserIdUseCase,
     UpdateTransactionUseCase,
 } from '../../use-cases/index.js'
@@ -56,4 +59,16 @@ export const makeUpdateTransactionController = () => {
         updateTransactionUseCase,
     )
     return updateTransactionController
+}
+
+export const makeDeleteTransactionController = () => {
+    const deleteTransactionRepository =
+        new PostgresDeleteTransactionRepository()
+    const deleteTransactionUseCase = new DeleteTransactionUseCase(
+        deleteTransactionRepository,
+    )
+    const deleteTransactionController = new DeleteTransactionController(
+        deleteTransactionUseCase,
+    )
+    return deleteTransactionController
 }
