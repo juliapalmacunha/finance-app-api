a6739f472830530ea3d834daf40059d8eba669f5
juliapalmacunha <juliapcunha13@gmail.com>
2026-05-29 08:27:08 -0300

    feat: ensure GetTransactionByIdRepository and UpdateTransactionRepository use prisma

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -1,31 +1,12 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresUpdateTransactionRepository {
-    async execute(TransactionId, updateTransactionParams) {
-        //criando listas para que os parametros
-        // entrem na posição certa e isso seja enviado para o postgres em linguagem sql
-        const updateFields = []
-        const updateValues = []
-
-        Object.keys(updateTransactionParams).forEach((key) => {
-            updateFields.push(`${key} = $${updateFields.length + 1}`)
-            updateValues.push(updateTransactionParams[key])
+    async execute(transactionId, updateTransactionParams) {
+        return await prisma.transaction.update({
+            where: {
+                id: transactionId,
+            },
+            data: updateTransactionParams,
         })
-
-        updateValues.push(TransactionId)
-
-        const updateQuery = `
-            UPDATE transactions 
-            SET ${updateFields.join(', ')} 
-            WHERE id = $${updateValues.length}
-            RETURNING *
-            `
-
-        const updatedTransaction = await PostgresHelper.query(
-            updateQuery,
-            updateValues,
-        )
-
-        return updatedTransaction[0]
     }
 }
