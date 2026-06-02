1af12d740197767442845e86b719ca81a07c9d7f
juliapalmacunha <juliapcunha13@gmail.com>
2026-06-02 17:03:02 -0300

    feat: add transformIgnoreParams to jest.config to faker pass

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,10 +1,16 @@
-
-
 /** @type {import('jest').Config} */
 const config = {
   coverageDirectory: "coverage",
   coverageProvider: "v8",
 
+  // 1. Evita o erro de permissão da pasta do Docker
+  watchPathIgnorePatterns: ['<rootDir>/.postgres-data/'],
+  modulePathIgnorePatterns: ['<rootDir>/.postgres-data/'],
+
+  // 2. Permite que o Babel converta o Faker moderno para o Jest entender
+  transformIgnorePatterns: [
+    'node_modules/(?!@faker-js/)',
+  ],
 };
 
 export default config;
