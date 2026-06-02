8fb1f9b6fe919574a1177d86b5fcba1497c0e5c7
juliapalmacunha <juliapcunha13@gmail.com>
2026-06-02 15:35:55 -0300

    test: ensure  is returned when user is created

---
+++ src/controllers/user/create-user.test.js
@@ -0,0 +1,33 @@
+import { CreateUserController } from './create-user.js'
+
+describe('Create User Controller', () => {
+    class CreateUserUseCaseStub {
+        execute(user) {
+            return user
+        }
+    }
+
+    it('should return 201 when creating a user successfully', async () => {
+        //arrange: prepara o teste para ser executado
+        const createUserController = new CreateUserController(
+            new CreateUserUseCaseStub(),
+        )
+
+        const httpRequest = {
+            body: {
+                first_name: 'teste',
+                last_name: 'teste',
+                email: 'teste@teste.com',
+                password: 'teste123',
+            },
+        }
+
+        //act: executa a ação que queremos testar
+        const result = await createUserController.execute(httpRequest)
+
+        //assert: verifica se o resultado é o esperado
+        expect(result.statusCode).toBe(201)
+        expect(result.body).not.toBeNull()
+        expect(result.body).not.toBeUndefined()
+    })
+})
