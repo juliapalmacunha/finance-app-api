69f7c7e6d8fe72111eebb66b5c237353303b9027
juliapalmacunha <juliapcunha13@gmail.com>
2026-06-02 16:01:05 -0300

    test: ensure is returned 400 if last_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -50,4 +50,24 @@ describe('Create User Controller', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if last_name is missing', async () => {
+        //arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'teste',
+                email: 'teste@teste.com',
+                password: 'teste123',
+            },
+        }
+
+        //act
+        const result = await createUserController.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
