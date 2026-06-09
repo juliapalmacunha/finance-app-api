5bb4e480ab08e1b00ba2b36ea17f431c076c9aae
juliapalmacunha <juliapcunha13@gmail.com>
2026-06-09 10:05:29 -0300

    test: ensure UpdateUserController returns 400 if invalid user email is provided

--- src/controllers/user/update-user.test.js
+++ src/controllers/user/update-user.test.js
@@ -38,4 +38,21 @@ describe('UpdateUserController', () => {
         //assert
         expect(result.statusCode).toBe(200)
     })
+
+    it('should return 400 if invalid user email is provided', async () => {
+        //arrange
+        const { sut } = makeSut()
+
+        //act
+        const result = await sut.execute({
+            ...httpRequest,
+            body: {
+                ...httpRequest.body,
+                email: 'invalid-email',
+            },
+        })
+
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
