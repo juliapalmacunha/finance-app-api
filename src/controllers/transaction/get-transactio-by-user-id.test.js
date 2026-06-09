13829e30ccc1e4d7c43979dc4bb2620dd8d3a73a
juliapalmacunha <juliapcunha13@gmail.com>
2026-06-09 13:19:46 -0300

    test: ensure GetTransactionByUserIdController returns 400 if userId param is invalid

--- src/controllers/transaction/get-transactio-by-user-id.test.js
+++ src/controllers/transaction/get-transactio-by-user-id.test.js
@@ -52,4 +52,17 @@ describe('Get Transaction By User Id Controller', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if userId param is invalid', async () => {
+        //arrange
+        const { sut } = makeSut()
+        //act
+        const result = await sut.execute({
+            query: {
+                userId: 'invalid-id',
+            },
+        })
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
