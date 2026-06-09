620492c9adc578f6f660dc337b81a0964b4f1cfd
juliapalmacunha <juliapcunha13@gmail.com>
2026-06-09 13:45:15 -0300

    test: ensure UpdateTransactionController returns 400 if transactionId is not valid

--- src/controllers/transaction/update-transaction.test.js
+++ src/controllers/transaction/update-transaction.test.js
@@ -45,4 +45,18 @@ describe('Update Transaction Controller', () => {
         //assert
         expect(result.statusCode).toBe(200)
     })
+
+    it('should return 400 if transactionId is not valid', async () => {
+        //arrange
+        const { sut } = makeSut()
+        //act
+        const result = await sut.execute({
+            ...httpRequest,
+            params: {
+                transactionId: 'invalid-id',
+            },
+        })
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
