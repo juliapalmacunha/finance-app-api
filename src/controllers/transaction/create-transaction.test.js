8533c7b1d260ed3a78870f4f487f3dd0ee7327a3
juliapalmacunha <juliapcunha13@gmail.com>
2026-06-09 10:58:46 -0300

    test: ensure CreateTransactionController returns 400 when missing name

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -48,4 +48,19 @@ describe('Create Transaction Controller', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 when missing name', async () => {
+        //arrange
+        const { sut } = makeSut()
+        //act
+        const result = await sut.execute({
+            ...httpRequest,
+            body: {
+                ...httpRequest.body,
+                name: undefined,
+            },
+        })
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
