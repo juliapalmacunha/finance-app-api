2f06ad3f6f9cb49cf0785da50cb60661fdf1c308
juliapalmacunha <juliapcunha13@gmail.com>
2026-06-09 10:59:58 -0300

    test: ensure CreateTransactionController returns 400 when missing date

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -63,4 +63,19 @@ describe('Create Transaction Controller', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 when missing date', async () => {
+        //arrange
+        const { sut } = makeSut()
+        //act
+        const result = await sut.execute({
+            ...httpRequest,
+            body: {
+                ...httpRequest.body,
+                date: undefined,
+            },
+        })
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
