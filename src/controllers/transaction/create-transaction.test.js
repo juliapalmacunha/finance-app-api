4826750063038281846c6ba7af265b484dc41e2a
juliapalmacunha <juliapcunha13@gmail.com>
2026-06-09 11:36:01 -0300

    test: ensure CreateTransactionController returns 400 if amount is not a valid number

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -161,4 +161,19 @@ describe('Create Transaction Controller', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if amount is not a valid number', async () => {
+        //arrange
+        const { sut } = makeSut()
+        //act
+        const result = await sut.execute({
+            ...httpRequest,
+            body: {
+                ...httpRequest.body,
+                amount: 'invalid-amount',
+            },
+        })
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
