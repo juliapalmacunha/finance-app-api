c24b529a484a27bed37b268e0962c02c75b5305f
juliapalmacunha <juliapcunha13@gmail.com>
2026-06-09 13:28:11 -0300

    test: ensure GetTransactionByUserIdController returns 500 whe use case throws an error

--- src/controllers/transaction/get-transactio-by-user-id.test.js
+++ src/controllers/transaction/get-transactio-by-user-id.test.js
@@ -83,4 +83,21 @@ describe('Get Transaction By User Id Controller', () => {
         //assert
         expect(result.statusCode).toBe(404)
     })
+
+    it('should return 500 when GetTransactionsByUserIdUseCase throws an error', async () => {
+        //arrange
+        const { sut, getTransactionByIdUseCase } = makeSut()
+        jest.spyOn(getTransactionByIdUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        //act
+        const result = await sut.execute({
+            query: {
+                userId: faker.string.uuid(),
+            },
+        })
+        //assert
+        expect(result.statusCode).toBe(500)
+    })
 })
