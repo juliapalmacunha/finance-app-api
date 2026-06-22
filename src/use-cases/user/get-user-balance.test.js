c97bb2d00e5641f24269cd02bba86d356aa79d36
juliapalmacunha <juliapcunha13@gmail.com>
2026-06-22 16:49:10 -0300

    test: ensure  GetUserBalanceUseCase throw UserNotFoundError if GetUserByIdRepository returns null

--- src/use-cases/user/get-user-balance.test.js
+++ src/use-cases/user/get-user-balance.test.js
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker'
 import { GetUserBalanceUseCase } from './get-user-balance'
+import { UserNotFoundError } from '../../errors/user'
 
 describe('GetUserBalanceUseCase', () => {
     const userBalance = {
@@ -51,5 +52,18 @@ describe('GetUserBalanceUseCase', () => {
 
         //assert
         expect(result).toEqual(userBalance)
+    })
+
+    it('should throw UserNotFoundError if GetUserByIdRepository returns null', async () => {
+        //arrange
+        const { sut, getUserByIdRepository } = makeSut()
+        jest.spyOn(getUserByIdRepository, 'execute').mockResolvedValueOnce(null)
+        const userId = faker.string.uuid()
+
+        //act
+        const promise = sut.execute(userId)
+
+        //assert
+        await expect(promise).rejects.toThrow(new UserNotFoundError(userId))
     })
 })
