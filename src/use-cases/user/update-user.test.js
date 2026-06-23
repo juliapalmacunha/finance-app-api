7ac046f61587ad9f1e262df1a47613292895ed89
juliapalmacunha <juliapcunha13@gmail.com>
2026-06-23 10:10:16 -0300

    test: ensure UpdateUserUseCas update user successfully with email and correct params

--- src/use-cases/user/update-user.test.js
+++ src/use-cases/user/update-user.test.js
@@ -61,4 +61,18 @@ describe('UpdateUserUseCase', () => {
         //assert
         expect(result).toBe(user)
     })
+
+    it('should update user successfully (with email)', async () => {
+        //arrange
+        const { sut, getUserByEmailRepository } = makeSut()
+        const executeSpy = jest.spyOn(getUserByEmailRepository, 'execute')
+        const email = faker.internet.email()
+        //act
+        const result = await sut.execute(faker.string.uuid(), {
+            email,
+        })
+        //assert
+        expect(result).toBe(user)
+        expect(executeSpy).toHaveBeenCalledWith(email)
+    })
 })
