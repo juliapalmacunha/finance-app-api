c5e2cb626dd0a9d3d8e2e8a345d015b70ce5bec6
juliapalmacunha <juliapcunha13@gmail.com>
2026-07-03 14:41:08 -0300

    fix(test): add delay to global setup to wait for database connection

--- jest.global-setup.js
+++ jest.global-setup.js
@@ -1,11 +1,17 @@
 //ferramente nativa do node para rodar processos no terminal de forma automatica
 import { execSync } from 'child_process'
 
+// Função auxiliar para forçar uma pausa
+const esperar = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
 export default async () => {
     console.log(
         '\n🐳 [Docker]: Garantindo que o container de testes está de pé...',
     )
     execSync('docker-compose up -d --wait postgres-test')
+
+    console.log('⏳ [Setup]: Aguardando o banco estabilizar as conexões...')
+    await esperar(2000) // Aguarda 2 segundos antes de prosseguir
 
     console.log('🚀 [Prisma]: Sincronizando o banco de testes com db push...')
     execSync('npx prisma db push')
