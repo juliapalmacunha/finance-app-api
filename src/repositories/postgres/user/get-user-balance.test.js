493c3f88c01fbdda1a387028b8911fce01eebe09
juliapalmacunha <juliapcunha13@gmail.com>
2026-07-07 10:16:31 -0300

    test: ensure GetUserBalanceRepository calls prisma transaction agregate 3 times with correct values

--- src/repositories/postgres/user/get-user-balance.test.js
+++ src/repositories/postgres/user/get-user-balance.test.js
@@ -4,6 +4,8 @@ import { PostgresGetUserBalanceRepository } from './get-user-balance'
 import { faker } from '@faker-js/faker'
 
 describe('GetUserBalanceRepository', () => {
+    const sut = new PostgresGetUserBalanceRepository()
+
     it('should get user balance on db', async () => {
         const user = await prisma.user.create({ data: fakeUser })
         await prisma.transaction.createMany({
@@ -60,13 +62,46 @@ describe('GetUserBalanceRepository', () => {
             ],
         })
 
-        const sut = new PostgresGetUserBalanceRepository()
-
         const result = await sut.execute(user.id)
 
         expect(result.earnings.toString()).toBe('15000')
         expect(result.expenses.toString()).toBe('2000')
         expect(result.investments.toString()).toBe('6000')
         expect(result.balance.toString()).toBe('7000')
+    })
+
+    it('should call prisma transaction aggregate three times with correct parameters', async () => {
+        jest.spyOn(prisma.transaction, 'aggregate')
+
+        await sut.execute(fakeUser.id)
+
+        expect(prisma.transaction.aggregate).toHaveBeenCalledTimes(3)
+        expect(prisma.transaction.aggregate).toHaveBeenNthCalledWith(1, {
+            where: {
+                user_id: fakeUser.id,
+                type: 'EXPENSE',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+        expect(prisma.transaction.aggregate).toHaveBeenNthCalledWith(2, {
+            where: {
+                user_id: fakeUser.id,
+                type: 'EARNING',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+        expect(prisma.transaction.aggregate).toHaveBeenNthCalledWith(3, {
+            where: {
+                user_id: fakeUser.id,
+                type: 'INVESTMENT',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
     })
 })
