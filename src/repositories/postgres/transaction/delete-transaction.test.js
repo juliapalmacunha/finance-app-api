7e32d45e2d0ff629ac4c25fbf5c7238255e3eedc
juliapalmacunha <juliapcunha13@gmail.com>
2026-07-07 20:14:05 -0300

    test: ensure PostgresDeleteTransactionRepository delete a transaction on db

---
+++ src/repositories/postgres/transaction/delete-transaction.test.js
@@ -0,0 +1,33 @@
+import { PostgresDeleteTransactionRepository } from './delete-transaction'
+import { transaction, user } from '../../../tests'
+import { prisma } from '../../../../prisma/prisma'
+import dayjs from 'dayjs'
+
+describe('PostgresDeleteTransactionRepository', () => {
+    it('should delete a transaction on db', async () => {
+        //arrange
+        await prisma.user.create({ data: user })
+        await prisma.transaction.create({
+            data: {
+                ...transaction,
+                user_id: user.id,
+            },
+        })
+        const sut = new PostgresDeleteTransactionRepository()
+
+        //act
+        const result = await sut.execute(transaction.id)
+
+        //assert
+        expect(result).not.toBeNull()
+        expect(result.user_id).toBe(user.id)
+        expect(dayjs(result.date).daysInMonth()).toBe(
+            dayjs(transaction.date).daysInMonth(),
+        )
+        expect(dayjs(result.date).month()).toBe(dayjs(transaction.date).month())
+        expect(dayjs(result.date).year()).toBe(dayjs(transaction.date).year())
+        expect(result.name).toBe(transaction.name)
+        expect(result.type).toBe(transaction.type)
+        expect(String(result.amount)).toBe(String(transaction.amount))
+    })
+})
