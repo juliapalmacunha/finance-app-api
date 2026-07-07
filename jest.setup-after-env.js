7709aa84166932666eca88e508b244f7302dcc5a
juliapalmacunha <juliapcunha13@gmail.com>
2026-07-07 10:04:29 -0300

    fix: ensure transactions are deleted first on jest setup file

--- jest.setup-after-env.js
+++ jest.setup-after-env.js
@@ -3,6 +3,6 @@ import { prisma } from './prisma/prisma'
 // Este bloco roda AUTOMATICAMENTE antes de cada arquivo de teste começar
 beforeEach(async () => {
     console.log('Limpando o banco de dados antes de cada teste...')
-    await prisma.user.deleteMany({})
     await prisma.transaction.deleteMany({})
+    await prisma.user.deleteMany({})
 })
