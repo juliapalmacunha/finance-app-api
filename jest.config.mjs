0c6dfb091d174833808def8b9f95e3696802d27e
juliapalmacunha <juliapcunha13@gmail.com>
2026-07-09 17:45:49 -0300

    test: ignore prisma and node_modules in watch mode

--- jest.config.mjs
+++ jest.config.mjs
@@ -12,8 +12,12 @@ const config = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup-after-env.js"],
 
   // 1. Evita o erro de permissão da pasta do Docker
-  watchPathIgnorePatterns: ['<rootDir>/.postgres-data/'],
-  modulePathIgnorePatterns: ['<rootDir>/.postgres-data/'],
+ watchPathIgnorePatterns: [
+    '<rootDir>/.postgres-data',
+    '<rootDir>/prisma',
+    '<rootDir>/node_modules'
+  ],
+  modulePathIgnorePatterns: ['<rootDir>/.postgres-data'],
 
   // 2. Permite que o Babel converta o Faker moderno para o Jest entender
   transformIgnorePatterns: [
