4e593820c6536809cb94110c7b9edb4ba16c7ed3
juliapalmacunha <juliapcunha13@gmail.com>
2026-07-10 08:10:01 -0300

    test: ensure TransactionControllerFactories returns valid DeleteTransactionController

--- src/factories/controllers/transaction.test.js
+++ src/factories/controllers/transaction.test.js
@@ -1,9 +1,11 @@
 import {
     CreateTransactionController,
     UpdateTransactionController,
+    DeleteTransactionController,
 } from '../../controllers'
 import {
     makeCreateTransactionController,
+    makeDeleteTransactionController,
     makeUpdateTransactionController,
 } from './transaction'
 
@@ -17,6 +19,12 @@ describe('TransactionControllerFactories', () => {
     it('should return a valid UpdateTransactionController instance', () => {
         expect(makeUpdateTransactionController()).toBeInstanceOf(
             UpdateTransactionController,
+        )
+    })
+
+    it('should return a valid DeleteTransactionController instance', () => {
+        expect(makeDeleteTransactionController()).toBeInstanceOf(
+            DeleteTransactionController,
         )
     })
 })
