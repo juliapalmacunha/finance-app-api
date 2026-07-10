b09067e2df94968a41cf2cbf6101b8e51256f9eb
juliapalmacunha <juliapcunha13@gmail.com>
2026-07-10 08:19:18 -0300

    test: ensure UserControllerFactories returns valid UpdateUserController

--- src/factories/controllers/user.test.js
+++ src/factories/controllers/user.test.js
@@ -1,8 +1,12 @@
-import { CreateUserController } from '../../controllers'
-import { makeCreateUserController } from './user'
+import { CreateUserController, UpdateUserController } from '../../controllers'
+import { makeCreateUserController, makeUpdateUserController } from './user'
 
 describe('UserControllerFactories', () => {
     it('should return a valid CreateUserController instance', () => {
         expect(makeCreateUserController()).toBeInstanceOf(CreateUserController)
+    })
+
+    it('should return a valid UpdateUserController instance', () => {
+        expect(makeUpdateUserController()).toBeInstanceOf(UpdateUserController)
     })
 })
