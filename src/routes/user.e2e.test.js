79a5d84e692e837ef23c9f2fb114d1c8f38ef07e
juliapalmacunha <juliapcunha13@gmail.com>
2026-07-13 16:26:50 -0300

    test: ensure GET USER  api/users returns 404 when a user is not found

--- src/routes/user.e2e.test.js
+++ src/routes/user.e2e.test.js
@@ -130,4 +130,13 @@ describe('UserE2eTests', () => {
             balance: '6000',
         })
     })
+
+    it('GET api/users/userId should return 404 when a user is not found', async () => {
+        const response = await request(app).get(
+            `/api/users/${faker.string.uuid()}`,
+        )
+
+        //assert
+        expect(response.status).toBe(404)
+    })
 })
