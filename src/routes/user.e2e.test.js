ebb979fa8fcfaebdc04139f2fc910b6eb054df06
juliapalmacunha <juliapcunha13@gmail.com>
2026-07-13 11:22:08 -0300

    test: ensure GET api/users returns 200 when a user is found

--- src/routes/user.e2e.test.js
+++ src/routes/user.e2e.test.js
@@ -1,9 +1,9 @@
 import request from 'supertest'
-import { app } from '../../index.js'
+import { app } from '../app.js'
 import { user } from '../tests/fixtures/user.js'
 
 describe('UserE2eTests', () => {
-    it('post /users should return 201 when a user is created', async () => {
+    it('POST api/users should return 201 when a user is created', async () => {
         //arrange
         const response = await request(app)
             .post('/api/users')
@@ -14,5 +14,22 @@ describe('UserE2eTests', () => {
 
         //assert
         expect(response.status).toBe(201)
+    })
+
+    it('GET api/users should return 200 when a user is found', async () => {
+        //arrange
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                id: undefined,
+                ...user,
+            })
+
+        //act
+        const response = await request(app).get(`/api/users/${createdUser.id}`)
+
+        //assert
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(createdUser)
     })
 })
