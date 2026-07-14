e24ea0df07e3bc7b76a6c08de330175fd7a8b4f4
juliapalmacunha <juliapcunha13@gmail.com>
2026-07-14 20:01:23 -0300

    test: ensure GET/ api/transacntions/userId returns 200 when get transaction by userId

--- src/routes/transactions.test.js
+++ src/routes/transactions.test.js
@@ -27,4 +27,33 @@ describe('UserE2eTests', () => {
         expect(response.body.type).toBe(transaction.type)
         expect(response.body.amount).toBe(String(transaction.amount))
     })
+
+    it('GET api/transactions/userId should return 200 when get transaction by userId', async () => {
+        //cria usuario
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+
+        //criar transação
+        const { body: createdTransaction } = await request(app)
+            .post(`/api/transactions`)
+            .send({
+                ...transaction,
+                user_id: createdUser.id,
+                id: undefined,
+            })
+
+        //chamar requisição
+        const response = await request(app).get(
+            `/api/transactions?userId=${createdUser.id}`,
+        )
+
+        //assert
+        //id da primeira transação [0] seja da transação que eu criei
+        expect(response.status).toBe(200)
+        expect(response.body[0].id).toBe(createdTransaction.id)
+    })
 })
