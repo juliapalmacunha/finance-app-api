ce09632319106010538f4cd38420a7946eb7c9f5
juliapalmacunha <juliapcunha13@gmail.com>
2026-07-14 20:53:47 -0300

    feat:  ensure UpdateTransactionRepository throws transactionNotFoundError if transaction is no found

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -1,12 +1,24 @@
+import { Prisma } from '@prisma/client'
 import { prisma } from '../../../../prisma/prisma.js'
+import { TransactionNotFoundError } from '../../../errors/transaction.js'
 
 export class PostgresUpdateTransactionRepository {
     async execute(transactionId, updateTransactionParams) {
-        return await prisma.transaction.update({
-            where: {
-                id: transactionId,
-            },
-            data: updateTransactionParams,
-        })
+        try {
+            return await prisma.transaction.update({
+                where: {
+                    id: transactionId,
+                },
+                data: updateTransactionParams,
+            })
+        } catch (error) {
+            if (error instanceof Prisma.PrismaClientKnownRequestError) {
+                const errorCode = error.code
+                if (errorCode === 'P2025') {
+                    throw new TransactionNotFoundError(transactionId)
+                }
+            }
+            throw error
+        }
     }
 }
