4b62e2b09c812d5369d257ea778f71e6e7eb1e27
juliapalmacunha <juliapcunha13@gmail.com>
2026-07-24 15:03:22 -0300

    refactor: move swagger docs route to app.js

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 import 'dotenv/config.js'
 import { app } from './src/app.js'
-import swaggerUi from 'swagger-ui-express'
-import fs from 'fs'
-import path from 'path'
 
 app.listen(process.env.PORT, () =>
     console.log('Server is running on port ' + process.env.PORT),
 )
-
-// Monta o caminho seguro: vai até a pasta do projeto -> docs -> swagger.json
-const swaggerPath = path.resolve('docs', 'swagger.json')
-
-// Agora o fs lê o arquivo usando o caminho correto e seguro
-const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf8'))
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import { transactionsRouter, usersRouter } from './routes/index.js'
+import swaggerUi from 'swagger-ui-express'
+import fs from 'fs'
+import path from 'path'
 
 export const app = express()
 
@@ -8,3 +11,11 @@ app.use(express.json())
 app.use('/api/users', usersRouter)
 
 app.use('/api/transactions', transactionsRouter)
+
+// Monta o caminho seguro: vai até a pasta do projeto -> docs -> swagger.json
+const swaggerPath = path.resolve('docs', 'swagger.json')
+
+// Agora o fs lê o arquivo usando o caminho correto e seguro
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf8'))
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
