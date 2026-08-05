0e79d70c7efe8b93d920d5e6a570e753bfcb13d7
juliapalmacunha <juliapcunha13@gmail.com>
2026-08-05 14:04:30 -0300

    feat: loginUserSchema created to valid the fields on LoginUserController

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -40,3 +40,24 @@ export const createUserSchema = z.object({
 export const updateUserSchema = createUserSchema.partial().strict({
     message: 'Some provided fields are not allowed',
 })
+
+export const loginUserSchema = z.object({
+    email: z
+        .string({
+            required_error: 'Email is required',
+        })
+        .email({
+            message: 'Please provide a valid email address',
+        })
+        .min(1, {
+            message: 'Email is required',
+        }),
+    password: z
+        .string({
+            required_error: 'Password is required',
+        })
+        .trim()
+        .min(6, {
+            message: 'Password must be at least 6 characters long',
+        }),
+})
