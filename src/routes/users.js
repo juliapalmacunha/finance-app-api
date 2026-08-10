aac7075630c095c9295718107a0bf5681c1725d2
juliapalmacunha <juliapcunha13@gmail.com>
2026-08-10 14:26:55 -0300

    feat: add login user route

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,7 @@ import {
     makeDeleteUserController,
     makeGetUserBalanceController,
     makeGetUserByIdController,
+    makeLoginUserController,
     makeUpdateUserController,
 } from '../factories/controllers/user.js'
 
@@ -45,5 +46,13 @@ usersRouter.delete('/:userId', async (request, response) => {
     const deleteUserController = makeDeleteUserController()
 
     const { statusCode, body } = await deleteUserController.execute(request)
+    response.status(statusCode).send(body)
+})
+
+//enviando login
+usersRouter.post('/login', async (request, response) => {
+    const loginUserController = makeLoginUserController()
+
+    const { statusCode, body } = await loginUserController.execute(request)
     response.status(statusCode).send(body)
 })
