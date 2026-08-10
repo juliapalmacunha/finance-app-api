bb8c176bfecc9c0a8c7d04258cbc335396e29d72
juliapalmacunha <juliapcunha13@gmail.com>
2026-08-10 13:23:25 -0300

    chore: setup eslint-plugin-import for ESM and update lint-staged

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,25 @@ import js from '@eslint/js'
 import globals from 'globals'
 import { defineConfig } from 'eslint/config'
 import prettier from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
 
 export default defineConfig([
     js.configs.recommended,
     prettier,
     {
         files: ['**/*.{js,mjs,cjs,ts}'],
+        plugins: {
+            import: importPlugin,
+        },
         languageOptions: {
             globals: {
                 ...globals.node,
                 ...globals.jest,
             },
+        },
+        rules: {
+            // Exige .js APENAS em imports relativos do seu código, ignorando pacotes do npm
+            'import/extensions': ['error', 'ignorePackages', { js: 'always' }],
         },
     },
 ])
