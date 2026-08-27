d0746d0ed722139471c4463ba67323cb41a85137
juliapalmacunha <juliapcunha13@gmail.com>
2026-08-27 16:13:47 -0300

    feat: ensure GetTransactionsByUserIdController calls use case with new correct params (from and to)

--- src/controllers/transaction/get-transactions-by-user-id.js
+++ src/controllers/transaction/get-transactions-by-user-id.js
@@ -33,7 +33,11 @@ export class GetTransactionsByUserIdController {
 
             //chamar o use case
             const transactions =
-                await this.getTransactionsByUserIdUseCase.execute(userId)
+                await this.getTransactionsByUserIdUseCase.execute(
+                    userId,
+                    from,
+                    to,
+                )
 
             //retornar resposta http
             return ok(transactions)
