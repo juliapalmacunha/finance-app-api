ae23ea4340e556923a7f4fc508a5aad7a983d771
juliapalmacunha <juliapcunha13@gmail.com>
2026-08-31 16:06:15 -0300

    feat: ensure /balance route receives from and to query params pass it to controller

--- src/routes/users.js
+++ src/routes/users.js
@@ -29,6 +29,10 @@ usersRouter.get('/balance', auth, async (request, response) => {
     const { statusCode, body } = await getUserBalanceController.execute({
         ...request,
         params: { userId: request.userId },
+        query: {
+            from: request.query.from,
+            to: request.query.to,
+        },
     })
     response.status(statusCode).send(body)
 })
