492cab33ecee63bc72c16b93f9a2903d1dd862fc
juliapalmacunha <juliapcunha13@gmail.com>
2026-09-02 11:55:06 -0300

    feat: ensure percentages per transaction is returned on api/balance route

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -53,15 +53,34 @@ export class PostgresGetUserBalanceRepository {
         const _totalInvestments = totalInvestments || new Prisma.Decimal(0)
         const _totalEarnings = totalEarnings || new Prisma.Decimal(0)
 
-        const balance = new Prisma.Decimal(
-            _totalEarnings - _totalExpenses - _totalInvestments,
-        )
+        const total = _totalEarnings
+            .plus(_totalExpenses)
+            .plus(_totalInvestments)
+
+        const balance = _totalEarnings
+            .minus(_totalExpenses)
+            .minus(_totalInvestments)
+
+        const earningsPercent = total.isZero()
+            ? new Prisma.Decimal(0)
+            : _totalEarnings.div(total).times(100).floor()
+
+        const expensesPercent = total.isZero()
+            ? new Prisma.Decimal(0)
+            : _totalExpenses.div(total).times(100).floor()
+
+        const investmentsPercent = total.isZero()
+            ? new Prisma.Decimal(0)
+            : _totalInvestments.div(total).times(100).floor()
 
         return {
             earnings: _totalEarnings,
             expenses: _totalExpenses,
             investments: _totalInvestments,
             balance: balance,
+            earningsPercent,
+            expensesPercent,
+            investmentsPercent,
         }
     }
 }
