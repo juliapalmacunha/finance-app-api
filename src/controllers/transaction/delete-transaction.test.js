096ce33e5e029aba840db9fe2d323ee250398532
juliapalmacunha <juliapcunha13@gmail.com>
2026-09-02 15:05:25 -0300

    test: ensure DeleteTransactionController receives userId as param

--- src/controllers/transaction/delete-transaction.test.js
+++ src/controllers/transaction/delete-transaction.test.js
@@ -90,14 +90,16 @@ describe('Delete Transaction Controller', () => {
         )
 
         const transactionId = faker.string.uuid()
+        const userId = faker.string.uuid()
 
         //act
         await sut.execute({
             params: {
                 transactionId,
+                user_id: userId,
             },
         })
         //assert
-        expect(executeSpy).toHaveBeenCalledWith(transactionId)
+        expect(executeSpy).toHaveBeenCalledWith(transactionId, userId)
     })
 })
