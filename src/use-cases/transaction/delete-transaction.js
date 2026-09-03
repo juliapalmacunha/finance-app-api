f460704ffb9a977a70faa14100101d6c98e92917
juliapalmacunha <juliapcunha13@gmail.com>
2026-09-03 12:55:03 -0300

    feat: ensure authenticated users can only delete existing transactions they own

--- src/use-cases/transaction/delete-transaction.js
+++ src/use-cases/transaction/delete-transaction.js
@@ -1,12 +1,27 @@
+import { ForbiddenError, TransactionNotFoundError } from '../../errors/index.js'
+
 export class DeleteTransactionUseCase {
-    constructor(deleteTransactionRepository) {
+    constructor(deleteTransactionRepository, getTransactionByIdRepository) {
         this.deleteTransactionRepository = deleteTransactionRepository
+        this.getTransactionByIdRepository = getTransactionByIdRepository
     }
 
-    async execute(transactionId) {
-        const transaction =
+    async execute(transactionId, userId) {
+        //verificar se a transacao pertence ao usuario
+        const foundedTransaction =
+            await this.getTransactionByIdRepository.execute(transactionId)
+
+        if (!foundedTransaction) {
+            throw new TransactionNotFoundError(transactionId)
+        }
+
+        if (foundedTransaction.user_id !== userId) {
+            throw new ForbiddenError()
+        }
+
+        const deletedTransaction =
             await this.deleteTransactionRepository.execute(transactionId)
 
-        return transaction
+        return deletedTransaction
     }
 }
